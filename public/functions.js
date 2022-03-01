873cd26872ecec3908b30e047a41b502a980eaff
Anonymous Dev <96826139+freecontenttest@users.noreply.github.com>
2022-03-01 16:35:59 +0530

    Update functions.js

--- public/functions.js
+++ public/functions.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-bot.catch(async (err, ctx) => {
-    console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
-    const chat_id = await getFromId(ctx);
-    ctx.telegram.sendMessage(chat_id, `Ooops, encountered an error ${ctx.updateType}: ${err.description ? err.description : err}`);
-});
+// bot.catch(async (err, ctx) => {
+//     console.log(`Ooops, encountered an error for ${ctx.updateType}`, err);
+//     const chat_id = await getFromId(ctx);
+//     ctx.telegram.sendMessage(chat_id, `Ooops, encountered an error ${ctx.updateType}: ${err.description ? err.description : err}`);
+// });
 
 const getFromId = async (ctx) => {
     if (ctx.from) {
@@ -20,23 +20,27 @@ const captalize = (string) => {
 }
 
 const notAllowedMessage = async (ctx) => {
-    if (ctx.updateType === 'callback_query') {
-        return ctx.telegram.answerCbQuery(ctx.callbackQuery.id, '⚠️  You\'re not allowed 🚫️ to select an option !!', true);
-    }
-    
-//     await ctx.deleteMessage();
-    const chat_id = await getFromId(ctx);
-    console.log('chat_id----', chat_id);
-    
-    return ctx.telegram.sendMessage(chat_id,`This bot is official private bot of @temp_demo\n\nDo you want this premium feature then message my owner @temp_demo`, {
-        reply_markup: {
-            inline_keyboard: [
-                [
-                    { text: "📂 Join Our Main Channel", url: 'https://t.me/my_channels_list_official' }
-                ]
-            ]
+    try {
+        if (ctx.updateType === 'callback_query') {
+            return ctx.telegram.answerCbQuery(ctx.callbackQuery.id, '⚠️  You\'re not allowed 🚫️ to select an option !!', true);
         }
-    });
+        
+        await ctx.deleteMessage();
+        const chat_id = await getFromId(ctx);
+        
+        return ctx.telegram.sendMessage(chat_id,`This bot is official private bot of @temp_demo\n\nDo you want this premium feature then message my owner @temp_demo`, {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        { text: "📂 Join Our Main Channel", url: 'https://t.me/my_channels_list_official' }
+                    ]
+                ]
+            }
+        });
+    }
+    catch (err) {
+        console.log('notAllowedMessage---err--', err)
+    }
 };
 
 module.exports = {
