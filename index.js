09666420f6a4673ca6e8ed07c3c3207bd919d142
Anonymous Dev <96826139+freecontenttest@users.noreply.github.com>
2022-03-01 16:15:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,8 @@ async function forwardToBinAndDeleteMessage (ctx, chat_id, message_id) {
             return await sendReply(ctx, ctx.from.id, error.description ? error.description : error);
         };
     }
-    await ctx.telegram.deleteMessage(chat_id, message_id);
+    console.log('error==>173');
+//     await ctx.telegram.deleteMessage(chat_id, message_id);
 };
 
 async function putToBeDeletedMessagesInQueue (ctx) {
@@ -468,6 +469,7 @@ bot.on('callback_query', async (ctx) => {
         const date2 = new Date((Date.now() / 1000 | 0) * 1000);
 
         if (date1 < date2) {
+            console.log('error==>472');
 //             await ctx.telegram.deleteMessage(ctx.chat.id, ctx.callbackQuery.message.message_id);
             return await db.deleteUserDataByMsgId({ user_id: ctx.from.id, chat_id: ctx.chat.id, message_id: ctx.callbackQuery.message.message_id })
         }
